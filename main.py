1a6d6db8bf07aa37bc05a6b25c18629d36a71632
Kang Hui <kanghuidl@qq.com>
2019-04-08 00:10:10 +0800

    refactor code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,20 +10,18 @@ import torch.nn as nn
 import torchvision as tv
 
 
-root = os.path.expanduser('~/.torch/datasets')
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-epochs = 20
+epochs = 10
 batch_size = 64
 
 nc, nz, ngf, ndf = 1, 64, 64, 64
-fixed_noises = torch.randn(batch_size, nz, 1, 1, device=device)
 
+root = os.path.expanduser('~/.torch/datasets')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-transform = tv.transforms.Compose([tv.transforms.Resize(32), tv.transforms.ToTensor()])
-
-dataset = tv.datasets.MNIST(root, transform=transform)
+dataset = tv.datasets.MNIST(root, transform=tv.transforms.ToTensor())
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+fixed_noises = torch.randn(batch_size, nz, 1, 1).to(device)
 
 
 def weights_init(m):
@@ -48,17 +47,17 @@ gnet = nn.Sequential(
     nn.BatchNorm2d(ngf),
     nn.ReLU(inplace=True),
     # (ngf, 16, 16)
-    nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=3, bias=False),
     nn.Sigmoid()
-    # (nc, 32, 32)
+    # (nc, 28, 28)
 )
 gnet = gnet.to(device)
 gnet.apply(weights_init)
 
 
 dnet = nn.Sequential(
-    # (nc, 32, 32)
-    nn.Conv2d(nc, ndf, kernel_size=4, stride=2, padding=1, bias=False),
+    # (nc, 28, 28)
+    nn.Conv2d(nc, ndf, kernel_size=4, stride=2, padding=3, bias=False),
     nn.LeakyReLU(0.2, inplace=True),
     # (ndf, 16, 16)
     nn.Conv2d(ndf, 2 * ndf, kernel_size=4, stride=2, padding=1, bias=False),
@@ -70,35 +69,40 @@ dnet = nn.Sequential(
     nn.LeakyReLU(0.2, inplace=True),
     # (4 * ndf, 4, 4)
     nn.Conv2d(4 * ndf, 1, kernel_size=4, stride=1, padding=0, bias=False),
+    nn.Sigmoid()
     # (1, 1, 1)
 )
 dnet = dnet.to(device)
 dnet.apply(weights_init)
 
 
-criterion = nn.BCEWithLogitsLoss()
+criterion = nn.BCELoss()
 goptimizer = torch.optim.Adam(gnet.parameters(), lr=0.0002, betas=(0.5, 0.999))
 doptimizer = torch.optim.Adam(dnet.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
-
 plt.ion()
 for epoch in range(epochs):
+    print('Epoch: {}/{}'.format(epoch + 1, epochs))
+    progbar = tqdm.trange(len(dataloader), ascii=True)
+
     for i, data in enumerate(dataloader):
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         # train with real
-        reals = data[0].to(device)
-        batch_size = reals.size(0)
+        real = data[0].to(device)
+        batch_size = real.size(0)
 
-        labels = torch.ones(batch_size, device=device)
-        outputs = dnet(reals).view(-1)
+        labels = torch.ones(batch_size).to(device)
+        outputs = dnet(real).view(-1)
         dloss_real = criterion(outputs, labels)
+        D_x = outputs.mean().item()
 
         # train with fake
-        noises = torch.randn(batch_size, nz, 1, 1, device=device)
-        fakes = gnet(noises)
-        labels = torch.zeros(batch_size, device=device)
-        outputs = dnet(fakes.detach()).view(-1)
+        noises = torch.randn(batch_size, nz, 1, 1).to(device)
+        fake = gnet(noises)
+        labels = torch.zeros(batch_size).to(device)
+        outputs = dnet(fake.detach()).view(-1) # not calc gnet's grad
         dloss_fake = criterion(outputs, labels)
+        D_G_z1 = outputs.mean().item()
 
         dloss = dloss_real + dloss_fake
         dnet.zero_grad()
@@ -106,23 +110,28 @@ for epoch in range(epochs):
         doptimizer.step()
 
         # (2) Update G network: maximize log(D(G(z)))
-        labels = torch.ones(batch_size, device=device)
-        outputs = dnet(fakes).view(-1)
+        labels = torch.ones(batch_size).to(device)
+        outputs = dnet(fake).view(-1)
         gloss = criterion(outputs, labels)
+        D_G_z2 = outputs.mean().item()
+
         gnet.zero_grad()
         gloss.backward()
         goptimizer.step()
 
-        print(epoch, i, dloss.item(), gloss.item())
+        postfix = 'dloss: {:.4f}, gloss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f}/{:.2f}'.format(dloss.item(), gloss.item(), D_x, D_G_z1, D_G_z2)
+        progbar.set_postfix_str(postfix)
+        progbar.update()
 
-        if i % 100 == 0:
-            images = gnet(fixed_noises)
-            images = images.detach().cpu()
-            images = tv.utils.make_grid(images)
-            images = images.numpy().transpose(1, 2, 0)
-            images[images < 0] = 0
-            plt.imshow(images)
-            plt.pause(0.1)
+        if i % 100 == 99:
+            fake = gnet(fixed_noises)
+            fake = fake.detach().cpu()
+            fake = tv.utils.make_grid(fake)
+            fake = fake.numpy().transpose(1, 2, 0)
+            plt.imshow(fake)
+            plt.pause(0.001)
+
+    progbar.close()
 
 plt.ioff()
 plt.show()
